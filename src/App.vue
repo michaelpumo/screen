b3d93601f2d1c872ea6c8ddd2ee1ad0ca3dfaaa7
Michael Pumo <hello@michaelpumo.com>
2024-03-12 12:59:52 +0000

    Update playground data.

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,22 @@
 <script lang="ts" setup>
 import Screen from '@/components/Screen.vue'
 
-const family = {
+const profile = {
   name: 'Michael',
   age: 40,
-  target: Infinity,
   children: false,
   about: {
     job: 'Web Developer',
-    hobbies: ['hiking', 'development', 'cooking', 'guitar', 'crypto'],
-    tagline: `Hello. I’m a freelance user interface developer. My name’s Michael Pumo. I craft user interfaces using modern frontend web technologies such as Vue and Nuxt.`
-  },
-  siblings: [
-    {
-      name: 'Vincent',
-      age: 43,
-      children: [
-        {
-          name: 'Hollie',
-          age: 21
-        },
-        {
-          name: 'Lily',
-          age: 5
-        }
-      ]
-    },
-    {
-      name: 'Laura',
-      age: 38,
-      children: [
-        {
-          name: 'Reuben',
-          age: 6
-        },
-        {
-          name: 'Rudi',
-          age: 4
-        }
-      ]
-    }
-  ]
+    hobbies: ['hiking', 'cooking', 'guitar'],
+    tagline: `I'm a freelance user interface developer.`
+  }
 }
 </script>
 
 <template>
   <div class="sl-p-5">
-    <Screen :log="family" />
+    <Screen
+      :log="profile"
+      label="My Profile" />
   </div>
 </template>
