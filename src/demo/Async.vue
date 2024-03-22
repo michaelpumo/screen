c384ef201246740cbe97c78cd49ddeaf7ac209e5
Michael Pumo <hello@michaelpumo.com>
2024-03-22 20:03:22 +0000

    Use different API example.

--- src/demo/Async.vue
+++ src/demo/Async.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
 import Screen from '@/components/Screen.vue'
 
-const endpoint = 'https://jsonplaceholder.org/users/1'
+const endpoint = 'https://dummyjson.com/products/1'
 const response = await fetch(endpoint)
 const json = await response.json()
-const user = json
+const product = json
 </script>
 
 <template>
   <Screen
-    :log="user"
-    label="User" />
+    :log="product"
+    label="Product" />
 </template>
