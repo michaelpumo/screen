14e6be8c90d9380942032280221e54e571f9b870
Michael Pumo <hello@michaelpumo.com>
2024-04-11 19:28:14 +0100

    LengthCount component.

---
+++ src/vue/components/LengthCount.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { getLength } from '../../utils/helpers'
+
+type Props = {
+  log?: unknown
+  type?: string
+}
+
+const { log = undefined, type = '' } = defineProps<Props>()
+</script>
+
+<template>
+  <span
+    data-component="LengthCount"
+    :class="`sl-rounded sl-font-bold sl-text-xs sl-bg-current sl-px-1 sl-bg-token-${type}`">
+    <span :class="`sl-text-primary`">
+      {{ getLength(log) }}
+    </span>
+  </span>
+</template>
