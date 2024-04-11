d481997d50a550fe15f81bde7468b5f3682fc03f
Michael Pumo <hello@michaelpumo.com>
2024-04-11 19:27:37 +0100

    Additional colour tokens.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import {
   scopedPreflightStyles,
-  isolateInsideOfContainer
+  isolateInsideOfContainer,
 } from 'tailwindcss-scoped-preflight'
 
 export default {
@@ -15,10 +15,11 @@ export default {
       sm: '0.8rem',
       base: '1rem',
       md: '1.2rem',
-      lg: '1.4rem'
+      lg: '1.4rem',
     },
     colors: {
       transparent: 'transparent',
+      current: 'currentColor',
       muted: 'var(--sl-muted)',
       type: 'var(--sl-type)',
       primary: 'var(--sl-primary)',
@@ -27,14 +28,28 @@ export default {
       quaternary: 'var(--sl-quaternary)',
       token: {
         array: 'var(--sl-array)',
+        arraybuffer: 'var(--sl-arraybuffer)',
+        asyncfunction: 'var(--sl-asyncfunction)',
+        asyncgeneratorfunction: 'var(--sl-asyncgeneratorfunction)',
         bigint: 'var(--sl-bigint)',
+        bigint64array: 'var(--sl-bigint64array)',
+        biguint64array: 'var(--sl-biguint64array)',
         boolean: 'var(--sl-boolean)',
         comment: 'var(--sl-comment)',
         date: 'var(--sl-date)',
+        error: 'var(--sl-error)',
+        file: 'var(--sl-file)',
+        float32array: 'var(--sl-purple)',
+        float64array: 'var(--sl-purple)',
         function: 'var(--sl-function)',
+        generatorfunction: 'var(--sl-generatorfunction)',
         htmlbodyelement: 'var(--sl-htmlbodyelement)',
+        int8array: 'var(--sl-int8array)',
+        int16array: 'var(--sl-int16array)',
+        int32array: 'var(--sl-int32array)',
         location: 'var(--sl-location)',
         map: 'var(--sl-map)',
+        math: 'var(--sl-math)',
         null: 'var(--sl-null)',
         number: 'var(--sl-number)',
         object: 'var(--sl-object)',
@@ -42,19 +57,26 @@ export default {
         set: 'var(--sl-set)',
         string: 'var(--sl-string)',
         symbol: 'var(--sl-symbol)',
+        uint8array: 'var(--sl-uint8array)',
+        uint16array: 'var(--sl-uint16array)',
+        uint32array: 'var(--sl-uint32array)',
+        uint8clampedarray: 'var(--sl-uint8clampedarray)',
         undefined: 'var(--sl-undefined)',
+        url: 'var(--sl-url)',
+        urlsearchparams: 'var(--sl-urlsearchparams)',
         weakmap: 'var(--sl-weakmap)',
-        weakset: 'var(--sl-weakset)'
-      }
+        weakref: 'var(--sl-weakref)',
+        weakset: 'var(--sl-weakset)',
+      },
     },
     fontFamily: {
-      mono: ['Geist Mono', 'Menlo', 'Monaco', 'Courier', 'monospace']
+      mono: ['Geist Mono', 'Menlo', 'Monaco', 'Courier', 'monospace'],
     },
-    extend: {}
+    extend: {},
   },
   plugins: [
     scopedPreflightStyles({
-      isolationStrategy: isolateInsideOfContainer('.sl-pf')
-    })
-  ]
+      isolationStrategy: isolateInsideOfContainer('.sl-pf'),
+    }),
+  ],
 }
